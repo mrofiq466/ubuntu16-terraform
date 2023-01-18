dc1ba78025501a335781e35ad92212288c706f93
mrofiq466 <59303583+mrofiq466@users.noreply.github.com>
2023-01-18 22:31:31 +0700

    Update ubuntu16.tf

--- ubuntu16.tf
+++ ubuntu16.tf
@@ -21,16 +21,16 @@ resource "libvirt_pool" "test" {
 
 resource "libvirt_cloudinit_disk" "commoninit" {
   name           = "commoninit.iso"
-  user_data      = data.template_file.user_data.rendered
-  network_config = data.template_file.network_config.rendered
+  user_data      = data.template_file.user1_data.rendered
+  network_config = data.template_file.network1_config.rendered
   pool           = libvirt_pool.test.name
 }
 
-data "template_file" "user_data" {
+data "template_file" "user1_data" {
   template = file("${path.module}/cloud_init.cfg")
 }
 
-data "template_file" "network_config" {
+data "template_file" "network1_config" {
   template = file("${path.module}/network_config.cfg")
 }
 
@@ -38,7 +38,6 @@ data "template_file" "network_config" {
 resource "libvirt_volume" "ubuntu-vda" {
   name   = "ubuntu-vda.qcow2"
   pool   = libvirt_pool.test.name
-  #source = "https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
   source = "https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.img"
   format = "qcow2"
 }
